cf6b6d56cb184ee867e5c92e2c364aa3ed5025d7
sushanyadav <53527664+sushanyadav@users.noreply.github.com>
2022-12-14 15:51:14 +0545

    1 file modified

--- numberInputs.ts
+++ numberInputs.ts
@@ -1,4 +1,7 @@
-// @jerald
+// These functions are used when we want to set the default initial value to undefined
+
+// In the scenario where there are no value (empty string), the input will return NaN.
+// Function transformNumberOnChange will convert the value to undefined instead of NaN
 export const transformNumberOnChange = (
   event: React.ChangeEvent<HTMLInputElement>
 ) => {
@@ -6,6 +9,8 @@ export const transformNumberOnChange = (
   return isNaN(numberValue) ? undefined : numberValue;
 };
 
+// This function is used to transform NaN and undefined value to empty value ('')
+// When we press on reset, the value will be set to undefined and without this function, it will show NaN instead of empty value ('')
 export const transformNumberValue = (value: number) => {
   return isNaN(value) ? '' : value;
 };
